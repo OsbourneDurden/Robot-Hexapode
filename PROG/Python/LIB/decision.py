5a0b386176338e2f788da21d7bdc4ea6c97438ef
a100kpm <iannis3toussaint@gmail.com>
2017-02-07 17:09:37 +0100

    Add files via upload

--- PROG/Python/LIB/decision.py
+++ PROG/Python/LIB/decision.py
@@ -1,6 +1,6 @@
 print('cmd_decision loaded')
 
-''' vÃ©rifier (notemment sur scout, que les orientations matrice sont tjs bon)'''
+''' vérifier (notemment sur scout, que les orientations matrice sont tjs bon)'''
 from math import sqrt
 '''from numpy import *'''
 import numpy as np
@@ -52,9 +52,37 @@ def envi(Carte_decision):
     Carte_decision[:,:,1]=BBB
     return Carte_decision    
     
+def autorrize(autorise,x,y,Carte_nav_dym)
+    if Carte_nav_dym(y,x,1)==0:
+        autorize=0
+    else:
+        autorize=1;
+    return autorize
+
+def choix(Carte_decision,Carte_nav,Carte,position,position_depart,graph,col,row,suppr)
+
+    BBB=Carte_decision[:,:,1]
+    [col2,row2]=np.nonzero(BBB==2) '''attention il semblerait que si il n'y a qu'un seul element il bug un peu'''
+    ''' (array([8]), array([2])) si un seul element contre (array([3, 8]), array([3, 2])) si deux elements
+    problem de tupple il semblerait'''
+    x=position[0];
+    y=position[1];
+    ignore = [];
+    for i in range(0,row.size):
+        if graph[i,:]==0:           ''' possible problem de notation ici '''
+            ignore=[ ignore , np.array([col(i) , row(i)]) ];
+
+    ignore = [ignore ; np.array([y , x ])]  ''' ????? peut être a enlever'''
+    ignore = [ignore ; suppr ];             ''' ????? peut être a enlever (mastic)'''
+
+
+    return xd,yd,ppoids1
+
+
 
 '''function [Carte_explo,Carte_decision]=scout(x,y,direction,Carte_explo,Carte,Carte_decision)'''
-def scout(x,y,direction,Carte_explo,Carte,Carte_decision):
+'''def scout(x,y,direction,Carte_explo,Carte,Carte_decision):
+la fonction scout est inutile puisse que l'on a la vision qui fait son job
     K=1
     ligne = np.size(Carte_explo,0)
     colonne = np.size(Carte_explo,1)
@@ -73,4 +101,5 @@ def scout(x,y,direction,Carte_explo,Carte,Carte_decision):
 
     if direction ==4:
         Carte_explo[y-1:y+1,x+1:x+2,:]=Carte[y-1:y+1,x+1:x+2,:]
-        Carte_decision[y-1:y+1,x+1:x+2,1]=ones([3,2],int)*K
+        Carte_decision[y-1:y+1,x+1:x+2,1]=ones([3,2],int)*K'''
+
